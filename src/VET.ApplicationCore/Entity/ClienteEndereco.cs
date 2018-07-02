dd40045a29542accce722dc285f3d81ff3769180
Breno Nogueira <brenozxc@hotmail.com>
2018-07-01 21:52:41 -0400

    Att 27/4

---
+++ src/VET.ApplicationCore/Entity/ClienteEndereco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VET.ApplicationCore.Entity
+{
+    public class ClienteEndereco
+    {
+
+        public int Id { get; set; }
+        public int ClienteId { get; set; }
+        public int EnderecoId { get; set; }
+        
+
+    }
+}
